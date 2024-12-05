5a2d908cda021c192b72d61221d74e0f8a0e97d0
kiryl <kiryl@DESKTOP-3T3FAOL>
2024-12-05 17:10:08 +0200

    Add project files.

---
+++ Lesson_13/Lesson_13.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <queue>
+#include <deque>
+#include "MyQueue.h"
+
+int main()
+{
+    myQueue::Queue<int> a;
+    a.push_back(7);
+    a.push_back(4);
+    a.push_back(9);
+    a.push_back(2);
+
+    while (!a.isEmpty())
+    {
+        cout << "First: " << a.first() << endl;
+        a.show();
+        a.pop_front();
+    }
+    a.show();
+}
